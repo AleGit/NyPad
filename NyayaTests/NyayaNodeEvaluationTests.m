8ad859b989682b23930835899cecf2e31fb2451e
Alexander Maringele <apple@maringele.at>
2012-07-22 16:55:54 +0200

    uncommented code removed

--- NyayaTests/NyayaNodeEvaluationTests.m
+++ NyayaTests/NyayaNodeEvaluationTests.m
@@ -13,62 +13,76 @@
 
 @implementation NyayaNodeEvaluationTests
 
+- (void)assertDerivations: (NyayaNode*)node expected:(BOOL) evaluation {
+    NyayaNode *imf = [node imf];
+    NyayaNode *nnf = [imf nnf];
+    NyayaNode *cnf = [nnf nnf];
+    NyayaNode *dnf = [imf dnf];
+    STAssertEquals(node.evaluation, evaluation, nil);
+    STAssertEquals(imf.evaluation, evaluation, nil);
+    STAssertEquals(nnf.evaluation, evaluation, nil);
+    STAssertEquals(cnf.evaluation, evaluation, nil);
+    STAssertEquals(dnf.evaluation, evaluation, nil);
+    
+
+}
+
 - (void)testEvaluation {
     NyayaNode *t = [NyayaNode atom:@"T"];
     NyayaNode *f = [NyayaNode atom:@"F"];
     
-    STAssertEquals(t.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(f.evaluation, (BOOL)FALSE, nil);
+    [self assertDerivations:t expected:TRUE];
+    [self assertDerivations:f expected:FALSE];
     
     NyayaNode *nt = [NyayaNode negation:t];
     NyayaNode *nf = [NyayaNode negation:f];
     NyayaNode *nnt = [NyayaNode negation:nt];
     NyayaNode *nnf = [NyayaNode negation:nf];
     
-    STAssertEquals(nt.evaluation, (BOOL)FALSE, nil);
-    STAssertEquals(nf.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(nnt.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(nnf.evaluation, (BOOL)FALSE, nil);
+    [self assertDerivations:nt expected:FALSE];
+    [self assertDerivations:nf expected:TRUE];
+    [self assertDerivations:nnt expected:TRUE];
+    [self assertDerivations:nnf expected:FALSE];
     
     NyayaNode *tat = [NyayaNode conjunction:t with:t];
     NyayaNode *taf = [NyayaNode conjunction:t with:nt];
     NyayaNode *fat = [NyayaNode conjunction:nt with:t];
     NyayaNode *faf = [NyayaNode conjunction:nt with:nt];
     
-    STAssertEquals(tat.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(taf.evaluation, (BOOL)FALSE, nil);
-    STAssertEquals(fat.evaluation, (BOOL)FALSE, nil);
-    STAssertEquals(faf.evaluation, (BOOL)FALSE, nil);
+    [self assertDerivations:tat expected:TRUE];
+    [self assertDerivations:taf expected:FALSE];
+    [self assertDerivations:fat expected:FALSE];
+    [self assertDerivations:faf expected:FALSE];
     
     NyayaNode *tot = [NyayaNode disjunction:nf with:nf];
     NyayaNode *tof = [NyayaNode disjunction:nf with:nt];
     NyayaNode *fot = [NyayaNode disjunction:nt with:t];
     NyayaNode *fof = [NyayaNode disjunction:nt with:nt];
     
-    STAssertEquals(tot.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(tof.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(fot.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(fof.evaluation, (BOOL)FALSE, nil);
+    [self assertDerivations:tot expected:TRUE];
+    [self assertDerivations:tof expected:TRUE];
+    [self assertDerivations:fot expected:TRUE];
+    [self assertDerivations:fof expected:FALSE];
     
     NyayaNode *tit = [NyayaNode implication:t with:t];
     NyayaNode *tif = [NyayaNode implication:t with:nt];
     NyayaNode *fit = [NyayaNode implication:nt with:t];
     NyayaNode *fif = [NyayaNode implication:nt with:f];
     
-    STAssertEquals(tit.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(tif.evaluation, (BOOL)FALSE, nil);
-    STAssertEquals(fit.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(fif.evaluation, (BOOL)TRUE, nil);
+    [self assertDerivations:tit expected:TRUE];
+    [self assertDerivations:tif expected:FALSE];
+    [self assertDerivations:fit expected:TRUE];
+    [self assertDerivations:fif expected:TRUE];
     
     NyayaNode *tbt = [NyayaNode bicondition:t with:t];
     NyayaNode *tbf = [NyayaNode bicondition:t with:f];
     NyayaNode *fbt = [NyayaNode bicondition:f with:t];
     NyayaNode *fbf = [NyayaNode bicondition:f with:nt];
     
-    STAssertEquals(tbt.evaluation, (BOOL)TRUE, nil);
-    STAssertEquals(tbf.evaluation, (BOOL)FALSE, nil);
-    STAssertEquals(fbt.evaluation, (BOOL)FALSE, nil);
-    STAssertEquals(fbf.evaluation, (BOOL)TRUE, nil);
+    [self assertDerivations:tbt expected:TRUE];
+    [self assertDerivations:tbf expected:FALSE];
+    [self assertDerivations:fbt expected:FALSE];
+    [self assertDerivations:fbf expected:TRUE];
     
     
 }
