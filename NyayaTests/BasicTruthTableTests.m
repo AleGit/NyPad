f5d9aef19c7446f463b9e65130d214079756bc29
Alexander Maringele <apple@maringele.at>
2012-08-06 10:05:00 +0200

    basic truth table tests completed

--- NyayaTests/BasicTruthTableTests.m
+++ NyayaTests/BasicTruthTableTests.m
@@ -13,154 +13,265 @@
 
 @implementation BasicTruthTableTests
 
-- (void)testTruthTableA {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"a"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
+- (void) assert:(NSString*)input truthTable:(NSString*)tt {
+    NyayaParser *parser = [[NyayaParser alloc] initWithString:input];
+    NyayaNode *ast = [parser parseFormula];
+    STAssertFalse(parser.hasErrors, input);
+    NyayaTruthTable *astTable = [[NyayaTruthTable alloc] initWithFormula:ast];
+    [astTable evaluateTable];
+    STAssertEqualObjects([astTable description],tt, input);
+}
+
+
+- (void)testTruthTableFandT {
+    NSString *tt = @""
+    "| F | T | F ∧ T |\n"
+    "| F | T | F     |";
+    
+    for (NSString *input in @[@"F&T"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+- (void)testTruthTableForT {
+    NSString *tt = @""
+    "| F | T | F ∨ T |\n"
+    "| F | T | T     |";
+    
+    for (NSString *input in @[@"F|T"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+
+- (void)testF {
+    
+    NSString *tt = @""
+    "| F |\n"
+    "| F |";
+    
+    for (NSString *input in @[@"F"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+- (void)testNotF {
+    
+    NSString *tt = @""
+    "| F | ¬F |\n"
+    "| F | T  |";
+    
+    for (NSString *input in @[@"!F", @"¬F"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+- (void)testT {
+    
+    NSString *tt = @""
+    "| T |\n"
+    "| T |";
+    
+    for (NSString *input in @[@"T"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+- (void)testNotT {
+    
+    NSString *tt = @""
+    "| T | ¬T |\n"
+    "| T | F  |";
+    
+    for (NSString *input in @[@"!T", @"¬T"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+
+
+- (void)testA {
+    
+    NSString *tt = @""
     "| a |\n"
     "| F |\n"
     "| T |";
     
-    STAssertEqualObjects([table description], expected, nil);
+    for (NSString *input in @[@"a"]) {
+        [self assert:input truthTable:tt];
+    }
 }
 
-- (void)testTruthTableF {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"F"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| F |\n"
-    "| F |";
+- (void)testNot {
     
-    STAssertEqualObjects([table description], expected, nil);
-}
-
-- (void)testTruthTableFandT {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"F&T"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| F | T | F ∧ T |\n"
-    "| F | T | F     |";
-    
-    STAssertEqualObjects([table description], expected, nil);
-}
-
-- (void)testTruthTableT {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"T"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| T |\n"
-    "| T |";
-    
-    STAssertEqualObjects([table description], expected, nil);
-}
-
-- (void)testTruthTableForT {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"F|T"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| F | T | F ∨ T |\n"
-    "| F | T | T     |";
-    
-    STAssertEqualObjects([table description], expected, nil);
-}
-
-- (void)testTruthTableNot {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"!x"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| x | ¬x |\n"
+    NSString *tt = @""
+    "| a | ¬a |\n"
     "| F | T  |\n"
     "| T | F  |";
     
-    STAssertEqualObjects([table description], expected, nil);
+    for (NSString *input in @[@"!a", @"¬a"]) {
+        [self assert:input truthTable:tt];
+    }
 }
 
-- (void)testTruthTableAnd {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"x & y"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| x | y | x ∧ y |\n"
+- (void)testNotNot {
+    
+    NSString *tt = @""
+    "| a | ¬a | ¬¬a |\n"
+    "| F | T  | F   |\n"
+    "| T | F  | T   |";
+    
+    for (NSString *input in @[@"!!a", @"¬¬a"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+- (void)testAnd {
+    
+    NSString *tt = @""
+    "| a | b | a ∧ b |\n"
     "| F | F | F     |\n"
     "| F | T | F     |\n"
     "| T | F | F     |\n"
     "| T | T | T     |";
     
-    STAssertEqualObjects([table description], expected, nil);
+    for (NSString *input in @[@"a&b", @"a∧b"]) {
+        [self assert:input truthTable:tt];
+    }
 }
 
-- (void)testTruthTableOr {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"x | y"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| x | y | x ∨ y |\n"
+- (void)testNotAnd {
+    
+    NSString *tt = @""
+    "| a | b | a ∧ b | ¬(a ∧ b) |\n"
+    "| F | F | F     | T        |\n"
+    "| F | T | F     | T        |\n"
+    "| T | F | F     | T        |\n"
+    "| T | T | T     | F        |";
+    
+    for (NSString *input in @[@"!(a&b)", @"¬(a∧b)"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+- (void)testOr {
+    
+    NSString *tt = @""
+    "| a | b | a ∨ b |\n"
     "| F | F | F     |\n"
     "| F | T | T     |\n"
     "| T | F | T     |\n"
     "| T | T | T     |";
     
-    STAssertEqualObjects([table description], expected, nil);
+    for (NSString *input in @[@"a|b", @"a∨b"]) {
+        [self assert:input truthTable:tt];
+    }
 }
 
-- (void)testTruthTableBic {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"x <> y"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| x | y | x ↔ y |\n"
+- (void)testNotOr {
+    
+    NSString *tt = @""
+    "| a | b | a ∨ b | ¬(a ∨ b) |\n"
+    "| F | F | F     | T        |\n"
+    "| F | T | T     | F        |\n"
+    "| T | F | T     | F        |\n"
+    "| T | T | T     | F        |";
+    
+    for (NSString *input in @[@"!(a|b)", @"¬(a∨b)"]) {
+        [self assert:input truthTable:tt];
+    }
+}
+
+- (void)testBic {
+    
+    NSString *tt = @""
+    "| a | b | a ↔ b |\n"
     "| F | F | T     |\n"
     "| F | T | F     |\n"
     "| T | F | F     |\n"
     "| T | T | T     |";
     
-    STAssertEqualObjects([table description], expected, nil);
+    for (NSString *input in @[@"(a<>b)", @"a↔b"]) {
+        [self assert:input truthTable:tt];
+    }
 }
 
-- (void)testTruthTableImp {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"x > y"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| x | y | x → y |\n"
+- (void)testNotBic {
+    
+    NSString *tt = @""
+    "| a | b | a ↔ b | ¬(a ↔ b) |\n"
+    "| F | F | T     | F        |\n"
+    "| F | T | F     | T        |\n"
+    "| T | F | F     | T        |\n"
+    "| T | T | T     | F        |";
+    
+    for (NSString *input in @[@"!(a<>b)", @"¬(a↔b)"]) {
+        [self assert:input truthTable:tt];
+    }
+    
+}
+
+- (void)testImp {
+    
+    NSString *tt = @""
+    "| a | b | a → b |\n"
     "| F | F | T     |\n"
     "| F | T | T     |\n"
     "| T | F | F     |\n"
     "| T | T | T     |";
     
-    STAssertEqualObjects([table description], expected, nil);
+    for (NSString *input in @[@"(a>b)",@"a→b"]) {
+        [self assert:input truthTable:tt];
+    }
+    
 }
 
-- (void)testTruthTableXor {
-    NyayaParser *parser = [[NyayaParser alloc] initWithString:@"x ^ y"];
-    NyayaNode *node = [parser parseFormula];
-    NyayaTruthTable *table = [[NyayaTruthTable alloc] initWithFormula:node];
-    [table evaluateTable];
-    NSString *expected = @""
-    "| x | y | x ⊻ y |\n"
+- (void)testNotImp {
+    
+    NSString *tt = @""
+    "| a | b | a → b | ¬(a → b) |\n"
+    "| F | F | T     | F        |\n"
+    "| F | T | T     | F        |\n"
+    "| T | F | F     | T        |\n"
+    "| T | T | T     | F        |";
+    
+    for (NSString *input in @[@"!(a>b)", @"¬(a→b)"]) {
+        [self assert:input truthTable:tt];
+    }
+    
+}
+
+- (void)testXor {
+    
+    NSString *tt = @""
+    "| a | b | a ⊻ b |\n"
     "| F | F | F     |\n"
     "| F | T | T     |\n"
     "| T | F | T     |\n"
     "| T | T | F     |";
     
-    STAssertEqualObjects([table description], expected, nil);
+    for (NSString *input in @[@"(a^b)", @"a⊻b"]) {
+        [self assert:input truthTable:tt];
+    }
+    
 }
+
+- (void)testNotXor {
+    
+    NSString *tt = @""
+    "| a | b | a ⊻ b | ¬(a ⊻ b) |\n"
+    "| F | F | F     | T        |\n"
+    "| F | T | T     | F        |\n"
+    "| T | F | T     | F        |\n"
+    "| T | T | F     | T        |";
+    
+    for (NSString *input in @[@"!(a^b)", @"¬(a⊻b)"]) {
+        [self assert:input truthTable:tt];
+    }
+    
+}
+
+#pragma mark - special tests
 
 - (void)testTruthTableXorEquals {
     NyayaParser *parser = [[NyayaParser alloc] initWithString:@"x ^ y"];
@@ -171,21 +282,21 @@
     NyayaTruthTable *table2 = [[NyayaTruthTable alloc] initWithFormula:node];
     [table1 evaluateTable];
     [table2 evaluateTable];
-    NSString *expected1 = @""
+    NSString *tt1 = @""
     "| x | y | x ⊻ y |\n"
     "| F | F | F     |\n"
     "| F | T | T     |\n"
     "| T | F | T     |\n"
     "| T | T | F     |";
-    NSString *expected2 = @""
+    NSString *tt2 = @""
     "| a | b | a ⊻ b |\n"
     "| F | F | F     |\n"
     "| F | T | T     |\n"
     "| T | F | T     |\n"
     "| T | T | F     |";
     
-    STAssertEqualObjects([table1 description], expected1, nil);
-    STAssertEqualObjects([table2 description], expected2, nil);
+    STAssertEqualObjects([table1 description], tt1, nil);
+    STAssertEqualObjects([table2 description], tt2, nil);
     STAssertFalse([table1 isEqual:table2], nil);
 }
 
