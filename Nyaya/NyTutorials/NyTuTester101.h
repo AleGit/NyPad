5fdd8c81786c289dd28dec7d46b33af9b79b62db
Alexander Maringele <apple@maringele.at>
2012-08-30 20:49:20 +0200

    deletions

---
+++ Nyaya/NyTutorials/NyTuTester101.h
@@ -0,0 +1,13 @@
+//
+//  NyTuTester101.h
+//  Nyaya
+//
+//  Created by Alexander Maringele on 30.08.12.
+//  Copyright (c) 2012 private. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NyTuTester101 : NSObject
+
+@end
