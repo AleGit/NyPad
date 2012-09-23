9250cfece3ffba59eb71a26074a8725327c9aa34
Alexander Maringele <github@maringele.at>
2012-09-23 17:22:23 +0200

    deletions

---
+++ Nyaya/BDD/NyNode.h
@@ -0,0 +1,13 @@
+//
+//  NyNode.h
+//  Nyaya
+//
+//  Created by Alexander Maringele on 23.09.12.
+//  Copyright (c) 2012 private. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NyNode <NSObject>
+
+@end
