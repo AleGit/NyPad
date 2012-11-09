748da55da3ca6ba3e227d05066091bb4685bfeb1
Alexander Maringele <ama@Charon.local>
2012-11-09 20:45:58 +0100

    bugfix

--- Nyaya/NyBoolTool/TruthTable+HTML.m
+++ Nyaya/NyBoolTool/TruthTable+HTML.m
@@ -15,7 +15,7 @@
 - (NSString*)htmlDescription {
     
     NSString *template = @"<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>"
-    "<html lang="en"><head><meta http-equiv='content-type' content='text/html; charset=UTF-8'>"
+    "<html><head><meta http-equiv='content-type' content='text/html; charset=UTF-8'>"
     "<title>Nyāya :: TruthTable</title>"
     // "<link rel=\"stylesheet\" type=\"text/css\" href=\"format.css\"></head>"
     "    <style type='text/css'>"
